a41a7bd97bd055b63de265edf2b6ffb67f5de104
PsyAssist Dev <psyassist@dev.com>
2026-07-22 12:53:47 -0300

    style: renomeia o botao de voz para Resumir Prontuario por Voz

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PsyAssist Service Worker - Cache & Offline Strategy
-const CACHE_NAME = 'psyassist-v44';
+const CACHE_NAME = 'psyassist-v45';
 
 // Recursos essenciais para cache (app funciona offline depois do primeiro acesso)
 const ASSETS_TO_CACHE = [
