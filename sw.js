fe928a252c3e2a404dab51ed50e25fd822bf62bd
PsyAssist Dev <psyassist@dev.com>
2026-07-22 13:17:16 -0300

    fix: restaura o botao de Salvar Prontuario por Voz que foi desfeito por um checkout acidental no HTML

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PsyAssist Service Worker - Cache & Offline Strategy
-const CACHE_NAME = 'psyassist-v46';
+const CACHE_NAME = 'psyassist-v47';
 
 // Recursos essenciais para cache (app funciona offline depois do primeiro acesso)
 const ASSETS_TO_CACHE = [
